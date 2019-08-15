00c2d33881a9ab82e6a97b97c1f737dff574b9ad
BryanNilsen <bryannilsen@gmail.com>
2019-08-15 15:28:47 -0500

    initial commit

---
+++ src/components/employee/EmployeeCard.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class EmployeeCard extends Component {
+  render() {
+    return (
+      <div className="card">
+        <div className="card-content">
+          <h3>Employee: </h3>
+          <p>Info</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default EmployeeCard;
