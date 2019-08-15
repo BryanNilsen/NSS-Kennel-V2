90322838b7d2f65e8a2561dadf7b20420092c21b
BryanNilsen <bryannilsen@gmail.com>
2019-08-15 15:53:13 -0500

    completed through chapter 3

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,10 +1,10 @@
 import { Route } from 'react-router-dom'
 import React, { Component } from 'react'
 import Home from './home/Home'
-import AnimalCard from './animal/AnimalCard'
+import AnimalList from './animal/AnimalList'
 //only include these once they are built - previous practice exercise
-import LocationCard from './location/LocationCard'
-import EmployeeCard from './employee/EmployeeCard'
+import LocationList from './location/LocationList'
+import EmployeeList from './employee/EmployeeList'
 import OwnerCard from './owner/OwnerCard'
 
 
@@ -17,13 +17,13 @@ class ApplicationViews extends Component {
           return <Home />
         }} />
         <Route path="/animals" render={(props) => {
-          return <AnimalCard />
+          return <AnimalList />
         }} />
         <Route path="/locations" render={(props) => {
-          return <LocationCard />
+          return <LocationList />
         }} />
         <Route path="/employees" render={(props) => {
-          return <EmployeeCard />
+          return <EmployeeList />
         }} />
         <Route path="/owners" render={(props) => {
           return <OwnerCard />
